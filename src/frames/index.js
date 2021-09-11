ed04f1ec6470f3bfd766449c6ce5277babdab51d
Ruilin Ma <ruilin3@ualberta.ca>
2021-09-11 15:46:42 -0600

    map module added

--- src/frames/index.js
+++ src/frames/index.js
@@ -1,17 +1,50 @@
-import { Layout, Menu, Breadcrumb, Card } from 'antd';
+import { DatePicker, Space, Form, Layout, Menu, Input, InputNumber, Breadcrumb, Card, Modal, Button } from 'antd';
 import {withRouter} from 'react-router-dom';
+import React, { useState } from 'react';
 
 
 const { Header, Content, Footer } = Layout;
+const { RangePicker } = DatePicker;
+
 
 function Frame(props){
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+      };
+
+    const validateMessages = {
+        required: '${label} is required!',
+        types: {
+          email: '${label} is not a valid email!',
+          number: '${label} is not a valid number!',
+        },
+        number: {
+          range: '${label} must be between ${min} and ${max}',
+        },
+      };
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    
     return (
     <Layout>
     <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
       <div className="logo" />
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">Add new activity?</Menu.Item>
+        <Menu.Item key="1" onClick={showModal}>Add new activity?</Menu.Item>
         {/* <Menu.Item key="2">nav 2</Menu.Item>
         <Menu.Item key="3">nav 3</Menu.Item> */}
       </Menu>
@@ -29,6 +62,30 @@ function Frame(props){
       </div>
       {props.children}
     </Content>
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            <Form {...layout} name="nest-messages" validateMessages={validateMessages}>
+            <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
+                <Input />
+            </Form.Item>
+            <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
+                <Input />
+            </Form.Item>
+            <Form.Item name={['user', 'timePeriod']} label="Time Period" >
+                <RangePicker showTime />
+            </Form.Item>
+            <Form.Item name={['user', 'eventName']} label="Event Name">
+                <Input />
+            </Form.Item>
+            <Form.Item name={['user', 'introduction']} label="Introduction">
+                <Input.TextArea />
+            </Form.Item>
+            <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                <Button type="primary" htmlType="submit">
+                Submit
+                </Button>
+            </Form.Item>
+            </Form>
+      </Modal>
     <Footer style={{ textAlign: 'center' }}>Hechthon Project ©2021 Created by :)</Footer>
   </Layout>)
 }
